1b746eda844ea2e2fc17d38f30c1acdb4bd1b3b0
gureks <gurek15033@iiitd.ac.in>
2017-06-27 23:30:17 +0530

    fixed Scroll error

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 jQuery(function($) {'use strict',
 
-	
+
 	// all Parallax Section
 	$(window).load(function(){'use strict',
 		$("#services").parallax("50%", 0.3);
 		$("#clients").parallax("50%", 0.3);
 	});
-	
+
 	// portfolio filter
 	$(window).load(function(){'use strict',
 		$portfolio_selectors = $('.portfolio-filter >li>a');
@@ -16,7 +16,7 @@ jQuery(function($) {'use strict',
 				itemSelector : '.col-sm-3',
 				layoutMode : 'fitRows'
 			});
-			
+
 			$portfolio_selectors.on('click', function(){
 				$portfolio_selectors.removeClass('active');
 				$(this).addClass('active');
@@ -26,7 +26,7 @@ jQuery(function($) {'use strict',
 			});
 		}
 	});
-	
+
 	//Pretty Photo
 	 $("a[data-gallery^='prettyPhoto']").prettyPhoto({
 	  social_tools: false
@@ -42,18 +42,6 @@ jQuery(function($) {'use strict',
 		},'json');
 		return false;
 	});
-
-
-	// Navigation Scroll
-	$(window).scroll(function(event) {
-		Scroll();
-	});
-
-	$('.navbar-collapse ul li a').click(function() {  
-		$('html, body').animate({scrollTop: $(this.hash).offset().top - 79}, 1000);
-		return false;
-	});
-
 });
 
 // Preloder script
@@ -74,29 +62,6 @@ jQuery(window).load(function(){'use strict';
 
 });
 
-
-// User define function
-function Scroll() {
-	var contentTop      =   [];
-	var contentBottom   =   [];
-	var winTop      =   $(window).scrollTop();
-	var rangeTop    =   200;
-	var rangeBottom =   500;
-	$('.navbar-collapse').find('.scroll a').each(function(){
-		contentTop.push( $( $(this).attr('href') ).offset().top);
-		contentBottom.push( $( $(this).attr('href') ).offset().top + $( $(this).attr('href') ).height() );
-	})
-	$.each( contentTop, function(i){
-		if ( winTop > contentTop[i] - rangeTop ){
-			$('.navbar-collapse li.scroll')
-			.removeClass('active')
-			.eq(i).addClass('active');			
-		}
-	})
-
-};
-
-
 	// Skill bar Function
 
 	jQuery(document).ready(function(){
@@ -106,5 +71,3 @@ function Scroll() {
 		},6000);
 	});
 });
-
-
